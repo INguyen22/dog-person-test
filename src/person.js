db33f184a87019394f20c1f65daa42c4756ef590
Ivy Nguyen <animeweirdo24@gmail.com>
2022-06-03 13:19:45 -0600

    Do dog-person test

--- src/person.js
+++ src/person.js
@@ -1,11 +1,28 @@
-// var Dog = require("./dog.js");
+var Dog = require("./dog.js");
 
 class Person {
-  constructor() {
-
+  constructor(name, dog) {
+    this.name = name
+    this.dog = dog
   }
-
-
+  fillFoodBowl() {
+    this.dog.eat()
+  }
+  throwBall() {
+    if (this.dog.fetchBall() === `This is fun!`) {
+      return `${this.dog.name} loves playing fetch!`
+      }
+      else {
+        return `Oh, I think ${this.dog.name} is too tired. I guess we won\'t play fetch right now.`
+      }
+    }
+  adoptAPup(name, age) {
+    if (this.dog !== undefined) {
+      return `You can\'t adopt Chloe. You already have ${this.dog.name}!`
+    }
+    var puppy = new Dog({ name: name, age: age})
+    this.dog = puppy
+  }
 }
 
 module.exports = Person;
